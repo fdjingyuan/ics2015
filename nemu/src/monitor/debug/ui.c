b658a9eb43e816cf5952d324861be04dab58bc7c
Liu Jingyuan <15307130385@fudan.edu.cn>
2017-04-01 15:06:41 +0800

    >  compile NEMU 15307130385 ljy Linux ljy-VirtualBox 4.2.0-27-generic #32~14.04.1-Ubuntu SMP Fri Jan 22 15:32:26 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  15:06:41 up 54 min,  2 users,  load average: 0.18, 0.16, 0.14 680dc8f468dff9b2a56e9a4c9b1d45a8ed0e10a5

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -43,7 +43,7 @@ static int cmd_si(char *args);
 
 static int cmd_info(char *args);
 
-//static int cmd_x(char *args);
+static int cmd_x(char *args);
 /*statement of other command
 static int cmd_p(char *args);
 
@@ -65,7 +65,7 @@ static struct {
 	{ "q", "Exit NEMU", cmd_q },
 	{ "si", "Single step the instruction", cmd_si },
 	{ "info", "Print regInfo or watchPointInfo", cmd_info },
-	//{ "x", "Scan memory", cmd_x },
+	{ "x", "Scan memory", cmd_x },
 	/*other command
 	{ "p", "Caculate and print expresstion", cmd_p },
 	{ "w", "Set new watchpoint", cmd_w },
@@ -117,8 +117,13 @@ static int cmd_si(char *args){
 		  //sscanf:if true,return the number of parameters
 				printf("Invalid number\n");
 		  }
+			else if(n<=0){
+				printf("Please input a positive number\n");
+				return 0;
+			}
 		  else if(n>10){
-				printf("the single step cannot exceed 10");
+				printf("the single step cannot exceed 10\n");
+				return 0;
 			}
 			else{
 				cpu_exec(n);
@@ -148,12 +153,12 @@ static int cmd_info(char *args){
 		  printf("\n");
 			//16bit register
 			for(i=0;i<8;i++){
-				printf("%s:\t0x%#x\t%d\n",regsw[i],reg_w(i),reg_w(i));}
+				printf("%s:\t0x%x\t%d\n",regsw[i],reg_w(i),reg_w(i));}
 		  printf("\n");
 			//8bit register
 			for(i=0;i<4;i++){
-		    printf("%s:\t0x%#x\t%d\t",regsb[i|4],reg_b(i|4),reg_b(i|4));
-				printf("%s:\t0x%#x\t%d\n",regsb[i],reg_b(i),reg_b(i));}			
+		    printf("%s:\t0x%x\t%d\t",regsb[i|4],reg_b(i|4),reg_b(i|4));
+				printf("%s:\t0x%x\t%d\n",regsb[i],reg_b(i),reg_b(i));}			
 		}
 		/*//print the information of the watchpoint
 		else if(strcmp(arg,"w")==0)
@@ -170,17 +175,36 @@ static int cmd_info(char *args){
 		}
 	return 0;
 }
-/*
+
 static int cmd_x(char* args){
-
-
-
-
-
-
-
-
-}*/
+  char* arg = strtok(NULL," ");
+	int ins=0;
+	if(arg == NULL){
+		printf("Please input length\n");
+		return 0;
+	}
+	else if(sscanf(arg,"%d",&ins)==0){
+		printf("Invalid number\n");
+		return 0;
+	}
+	else{
+		char* addr = strtok(NULL," ");
+		if(addr == NULL){
+			printf("Please input start address\n");
+			return 0;
+		}
+		else{
+			swaddr_t start_addr;
+			sscanf(addr,"%x",&start_addr);
+			int i;
+			for(i=0;i<ins;i++){
+				printf("%d\t0x%x:0x%x\n",i+1,start_addr,swaddr_read(start_addr,4));
+				start_addr+=4;
+			}
+		}
+	}
+	return 0;
+}
 
 void ui_mainloop() {
 	while(1) {
