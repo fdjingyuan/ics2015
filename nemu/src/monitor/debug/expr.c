7d891d93c6241e7685690bc53ab1011e6fe845dd
Liu Jingyuan <15307130385@fudan.edu.cn>
2017-04-09 15:13:20 +0800

    >  compile NEMU 15307130385 ljy Linux ljy-VirtualBox 4.2.0-27-generic #32~14.04.1-Ubuntu SMP Fri Jan 22 15:32:26 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  15:13:20 up  6:31,  2 users,  load average: 0.56, 0.30, 0.25 82dc76f84a8dcc06fd5cbec308597f6a3bcb8c

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -102,6 +102,7 @@ static bool make_token(char *e) {
 				
 				char *substr_start = e + position;
 				int substr_len = pmatch.rm_eo;
+				printf("start=%c,len=%d\n",*substr_start,substr_len);
 
 				//Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s", i, rules[i].regex, position, substr_len, substr_len, substr_start);
 				position += substr_len;
