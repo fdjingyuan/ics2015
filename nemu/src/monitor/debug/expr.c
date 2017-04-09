ec0894b9d61175a784fe66d2821f34de31a426d4
Liu Jingyuan <15307130385@fudan.edu.cn>
2017-04-09 14:53:48 +0800

    >  compile NEMU 15307130385 ljy Linux ljy-VirtualBox 4.2.0-27-generic #32~14.04.1-Ubuntu SMP Fri Jan 22 15:32:26 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  14:53:48 up  6:11,  2 users,  load average: 0.44, 0.34, 0.25 d257ecd897125e68b53b110329d34384c8de633

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -194,7 +194,7 @@ uint32_t eval(uint32_t p,uint32_t q){
 	{
 		uint32_t n;
 		if(tokens[q].type==INT_x)
-			return strtoull(tokens[q].str,NULL,16);
+			n = strtoull(tokens[q].str,NULL,16);
 		else if(tokens[q].type==INT_d) 
 			sscanf(tokens[q].str, "%d", &n);
 		else
