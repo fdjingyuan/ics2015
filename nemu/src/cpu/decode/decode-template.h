94c5c93a1e8d262ca058867839f121f775ee12de
15307130385-Liu Jingyuan <15307130385@fudan.edu.cn>
2017-05-13 23:43:32 +0800

    modify decode

--- nemu/src/cpu/decode/decode-template.h
+++ nemu/src/cpu/decode/decode-template.h
@@ -24,16 +24,16 @@ make_helper(concat(decode_i_, SUFFIX)) {
 #if DATA_BYTE == 1 || DATA_BYTE == 4
 /* sign immediate */
 make_helper(concat(decode_si_, SUFFIX)) {
-	op_src->type = OP_TYPE_IMM;
+	
 
 	/* TODO: Use instr_fetch() to read ``DATA_BYTE'' bytes of memory pointed 
 	 * by ``eip''. Interpret the result as an signed immediate, and assign
 	 * it to op_src->simm.
-	 *
-	op_src->simm = ???
 	 */
-	panic("please implement me");
-
+	
+	//panic("please implement me");
+	op_src->type = OP_TYPE_IMM;
+	op_src->simm = (DATA_TYPE_S)instr_fetch(eip,DATA_BYTE);	
 	op_src->val = op_src->simm;
 
 #ifdef DEBUG
