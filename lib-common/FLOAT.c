956c2124513060cd9fd9021c2040e9676c80bc6f
Liu Jingyuan <15307130385@fudan.edu.cn>
2017-05-21 11:12:25 -0700

    modify float.h

--- lib-common/FLOAT.c
+++ lib-common/FLOAT.c
@@ -1,23 +1,19 @@
 #include "FLOAT.h"
 
 FLOAT F_mul_F(FLOAT a, FLOAT b) {
-	nemu_assert(0);
-	return 0;
+	
 }
 
 FLOAT F_div_F(FLOAT a, FLOAT b) {
-	nemu_assert(0);
-	return 0;
+	
 }
 
 FLOAT f2F(float a) {
-	nemu_assert(0);
-	return 0;
+	
 }
 
 FLOAT Fabs(FLOAT a) {
-	nemu_assert(0);
-	return 0;
+	
 }
 
 FLOAT sqrt(FLOAT x) {
